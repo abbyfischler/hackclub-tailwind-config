6004461f7be0b69fe076f41460bc6a2f1a8c9237
Abby Fischler <abigail.fischler@gmail.com>
2022-02-02 17:07:10 -0800

    added colors
    modes, initialColorModeName, and useColorSchemeMediaQuery questions in the comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,7 +38,58 @@ module.exports = {
         lg: ['20px', '28px'],
         xl: ['24px', '32px'],
       },
+      colors: {
+        'darker': '#121217',
+        'dark':'#17171d',
+        'darkless': '#252429',
+        'black': '#1f2d3d',
+        'steel': '#273444',
+        'slate': '#3c4858',
+        'muted': '#8492a6',
+        'smoke': '#e0e6ed',
+        'snow':  '#f9fafc',
+        'white': '#ffffff',
+        'red': '#ec3750',
+        'orange': '#ff8c37',
+        'yellow': '#f1c40f',
+        'green': '#33d6a6',
+        'cyan': '#5bc0de',
+        'blue': '#338eda',
+        'purple': '#a633d6',
+        'twitter': '#1da1f2',
+        'facebook': '#3b5998',
+        'instagram': '#e1306c',
+        'text': '#1f2d3d',
+        'background': '#ffffff',
+        'elevated': '#ffffff',
+        'sheet': '#f9fafc',
+        'sunken': '#e0e6ed',
+        'border': '#e0e6ed',
+        'placeholder': '#8492a6',
+        'secondary': '#3c4858',
+        'primary': '#ec3750',
+        'accent': '#338eda',
+      },
     },
   },
   plugins: [],
 }
+
+
+//       'initialColorModeName    ':     'light   ' // NEED TO ADD!!
+//     'useColorSchemeMediaQuery    ': true,
+//modes??
+/*
+"modes": {
+  "dark": {
+    "text": "#ffffff",
+    "background": "#17171d",
+    "elevated": "#252429",
+    "sheet": "#252429",
+    "sunken": "#121217",
+    "border": "#252429",
+    "placeholder": "#3c4858",
+    "secondary": "#8492a6",
+    "muted": "#8492a6",
+    "accent": "#5bc0de"
+  }*/
