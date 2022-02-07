f9ceae0fe60d087e588e205459c2cd092da1bd48
Abby Fischler <abigail.fischler@gmail.com>
2022-02-07 13:38:31 -0800

    Updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,45 +38,14 @@ module.exports = {
         lg: ['20px', '28px'],
         xl: ['24px', '32px'],
       },
-      colors: {
-        'darker': '#121217',
-        'dark':'#17171d',
-        'darkless': '#252429',
-        'black': '#1f2d3d',
-        'steel': '#273444',
-        'slate': '#3c4858',
-        'muted': '#8492a6',
-        'smoke': '#e0e6ed',
-        'snow':  '#f9fafc',
-        'white': '#ffffff',
-        'red': '#ec3750',
-        'orange': '#ff8c37',
-        'yellow': '#f1c40f',
-        'green': '#33d6a6',
-        'cyan': '#5bc0de',
-        'blue': '#338eda',
-        'purple': '#a633d6',
-        'twitter': '#1da1f2',
-        'facebook': '#3b5998',
-        'instagram': '#e1306c',
-        'text': '#1f2d3d',
-        'background': '#ffffff',
-        'elevated': '#ffffff',
-        'sheet': '#f9fafc',
-        'sunken': '#e0e6ed',
-        'border': '#e0e6ed',
-        'placeholder': '#8492a6',
-        'secondary': '#3c4858',
-        'primary': '#ec3750',
-        'accent': '#338eda',
-      },
     },
   },
   plugins: [],
 }
+fonts
 
+//change next theme  “initialColorModeName”: “light”
 
-//       'initialColorModeName    ':     'light   ' // NEED TO ADD!!
 //     'useColorSchemeMediaQuery    ': true,
 //modes??
 /*
