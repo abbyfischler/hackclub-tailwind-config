1d5b3e0aa0468bf3ce5bd7503fbb305378c53cf9
Abby Fischler <abigail.fischler@gmail.com>
2022-02-09 10:48:46 -0800

    Update _app.js
    worked on theme provider set up

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import '../styles/globals.css'
 import { ThemeProvider } from 'next-themes'
 
+// like this: 
+
+storageKey = 'theme';
+themes = ['light, dark'];
+
+
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider>
